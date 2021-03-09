63d7098139c1705126c427271f3f7ae937536a65
maudfabrello <ma.fabrello@gmail.com>
2021-03-09 14:33:31 +0100

    update

--- bin/seed.album.js
+++ bin/seed.album.js
@@ -10,37 +10,37 @@ const albums = [
 
   {
     title: "messengers",
-    releaseDate: new Date("19/06/2007"),
+    // releaseDate: new Date("19/06/2007"),
     artist:null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615215216/albums%20covers/august-cover_z3sml5.jpg",
   },
   {
     title: "wu tang forever",
-    releaseDate: new Date("06/03/1997"),
+    // releaseDate: new Date("06/03/1997"),
     artist:null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615217191/albums%20covers/wutang-cover_kycjgo.jpg",
   },
   {
     title: "wasting lights",
-    releaseDate: new Date("12/04/2011"),
+    // releaseDate: new Date("12/04/2011"),
     artist:null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615217251/albums%20covers/wastinglights-cover_kbln8z.jpg",
   },
   {
     title: "era vulgaris",
-    releaseDate: new Date("12/06/2007"),
+    // releaseDate: new Date("12/06/2007"),
     artist:null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615217473/albums%20covers/eravulgaris-cover_tveaug.jpg",
   },
   {
     title: "rage against the machine",
-    releaseDate: new Date("03/11/1992"),
+    // releaseDate: new Date("03/11/1992"),
     artist:null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615217558/albums%20covers/rageagainst-cover_olir3g.jpg",
   },
   {
     title: "white pony",
-    releaseDate: new Date("20/06/2000"),
+    // releaseDate: new Date("20/06/2000"),
     artist: null,
     cover: "https://res.cloudinary.com/dp9nyuimq/image/upload/v1615222314/albums%20covers/Whitepony-cover_kp3wvu.jpg",
   },
--- bin/seed.user.js
+++
@@ -1,8 +0,0 @@
-const user = [
-    {
-      username: "blabla",
-      password: "blibli",
-      email: true,
-      role: admin,
-    }];
-      // avatar: A VOIR PLUS TARD POUR AJOUTER IMAGE
