8feb567ed4ad8d4318bbc9414e7fd3f7ec5e610e
boubacar Sidibe <bouba.sidibe@gmail.com>
2021-03-09 14:27:44 +0100

    album done

--- models/album.js
+++ models/album.js
@@ -6,8 +6,8 @@ const albumSchema = new Schema(
     {
       title: String,
       releaseDate: Date,
-      label: { type: Schema.Types.ObjectId, ref: "label" }, 
-      // ref is the link to an other collection, here label
+      // label: { type: Schema.Types.ObjectId, ref: "label" }, 
+      // // ref is the link to an other collection, here label
       artist: { type: Schema.Types.ObjectId, ref: "artist" }, 
       // ref is the link to an other collection, here artist
       cover: {
