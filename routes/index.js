b25c400de8ac6d499622a755701d6be65129407c
Mathieu Jacob <mathieu.jacob@hotmail.fr>
2021-03-10 14:08:29 +0100

    upd css

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const AlbumModel = require("./../models/album");
 // const protectRoute = require("./../middlewares/thisProtector")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  res.render('index', { title: 'Project-Music' });
+router.get('/', async function(req, res, next) {
+  const Albums = await  AlbumModel.find();
+  res.render('index', { Albums });
 });
+
+router.get('index',async (req, res, next) => {
+  const Albums = await  AlbumModel.find();
+  res.render("index", { Albums });
+});
+
+// router.get("/create", async (req, res, next) => {
+//   const artists =  await ArtistModel.find();
+//   res.render("dashboard/albumCreate", { artists });
+// });
 
 router.get("/profile",  function (req, res) {
   res.render("Profile");
